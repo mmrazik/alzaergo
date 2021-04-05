81b622714c26356b19cdda4f83a7b73d2f6b5e13
Martin Mrazik <mrazik@gmail.com>
2021-04-05 17:31:38 +0200

    Add more test coverage and lcov

--- tests/AlzaEt1Ng_unittest.cpp
+++ tests/AlzaEt1Ng_unittest.cpp
@@ -1,4 +1,6 @@
 #include "gtest/gtest.h"
+#include <unistd.h>
+#include "gmock/gmock-matchers.h"
 #include "arduino-mock/Arduino.h"
 #include "arduino-mock/Serial.h"
 
@@ -16,7 +18,7 @@ class BasicTestFixture : public ::testing::Test {
     ArduinoMock *arduinoMock;
     SerialMock *serialMock;
 
-    void SetUp() {
+    virtual void SetUp() {
         arduinoMock = arduinoMockInstance();
         serialMock = serialMockInstance();
         EXPECT_CALL(*arduinoMock, pinMode(KEY_PIN, OUTPUT));
@@ -24,9 +26,27 @@ class BasicTestFixture : public ::testing::Test {
         EXPECT_CALL(*serialMock, begin(9600));
     }
 
-    void TearDown() {
+    virtual void TearDown() {
         releaseSerialMock();
         releaseArduinoMock();
+    }
+};
+
+class TestFixtureWithSerialData : public BasicTestFixture {
+    protected:
+    void SetSerialMessage(const int *response, int size) {
+        EXPECT_CALL(*serialMock, available())
+            .Times(size)
+            .WillRepeatedly(Return(true));
+
+        EXPECT_CALL(*serialMock, read())
+            .WillOnce(Return(response[0]))
+            .WillOnce(Return(response[1]))
+            .WillOnce(Return(response[2]))
+            .WillOnce(Return(response[3]))
+            .WillOnce(Return(response[4]))
+            .WillOnce(Return(response[5]));
+
     }
 };
 
@@ -42,6 +62,72 @@ TEST_F(BasicTestFixture, holdCommandStatus) {
     EXPECT_CALL(*arduinoMock, digitalWrite(KEY_PIN, 0));
     AlzaControl.holdCommand(AlzaET1Ng::Commands::Status);
     ASSERT_EQ(AlzaControl.getNextCommand(), AlzaET1Ng::Commands::Status);
+}
+
+TEST_F(BasicTestFixture, waitsForResponseAfterStartup) {
+    AlzaET1Ng::ControlPanel AlzaControl(Serial, KEY_PIN);
+    EXPECT_CALL(*serialMock, write(testing::An<uint8_t>())).Times(AlzaET1Ng::COMMAND_SIZE);
+    EXPECT_CALL(*arduinoMock, millis()).Times(2);
+    EXPECT_CALL(*serialMock, available()).WillOnce(Return(false));
+    AlzaControl.handleLoop();
+    ASSERT_EQ(AlzaControl.waitingForResponse(), true);
+}
+
+
+TEST_F(BasicTestFixture, waitForResponseTimesOut) {
+    AlzaET1Ng::ControlPanel AlzaControl(Serial, KEY_PIN);
+    EXPECT_CALL(*serialMock, write(testing::An<uint8_t>())).Times(AlzaET1Ng::COMMAND_SIZE);
+    EXPECT_CALL(*arduinoMock, millis())
+        .WillOnce(Return(0))
+        .WillOnce(Return(0));
+    EXPECT_CALL(*serialMock, available()).WillOnce(Return(false));
+    AlzaControl.handleLoop();
+    ASSERT_EQ(AlzaControl.waitingForResponse(), true);
+    EXPECT_CALL(*arduinoMock, millis()).WillOnce(Return(AlzaET1Ng::WAIT_FOR_RESPONSE_TIMEOUT + 1));
+    EXPECT_CALL(*serialMock, available()).WillOnce(Return(false));
+    AlzaControl.handleLoop();
+    ASSERT_EQ(AlzaControl.waitingForResponse(), false);
+}
+
+
+TEST_F(TestFixtureWithSerialData, handleIncomingResponse) {
+    AlzaET1Ng::ControlPanel AlzaControl(Serial, KEY_PIN);
+    // Corresponds to 76.5
+    int message[] = {AlzaET1Ng::RESPONSE_HEADER, 0x7, 0xfd, 0x6d,  0x10, 0x81};
+    SetSerialMessage(message, AlzaET1Ng::RESPONSE_SIZE);
+    for (int i=0; i < AlzaET1Ng::RESPONSE_SIZE; i++) {
+        AlzaControl.handleIncomingResponse();
+    }
+    int data[3];
+    AlzaControl.getBcdDisplay(data);
+    ASSERT_EQ(data[0], 0x7);
+    ASSERT_EQ(data[1], 0xfd);
+    ASSERT_EQ(data[2], 0x6d);
+
+    ASSERT_EQ(AlzaControl.getHeight(), 765);
+
+    char str_data[4];
+    AlzaControl.getBcdDisplayAsString(str_data);
+    ASSERT_EQ(str_data[0], '7');
+    ASSERT_EQ(str_data[1], '6');
+    ASSERT_EQ(str_data[2], '.');
+    ASSERT_EQ(str_data[3], '5');
+}
+
+
+TEST_F(TestFixtureWithSerialData, handleIncomingResponseWithWrongChecksum) {
+    AlzaET1Ng::ControlPanel AlzaControl(Serial, KEY_PIN);
+    // Corresponds to 76.5
+    int message[] = {AlzaET1Ng::RESPONSE_HEADER, 0x7, 0xfd, 0x6d,  0x10, 0x82};
+    SetSerialMessage(message, AlzaET1Ng::RESPONSE_SIZE);
+    for (int i=0; i < AlzaET1Ng::RESPONSE_SIZE; i++) {
+        AlzaControl.handleIncomingResponse();
+    }
+    int data[3];
+    AlzaControl.getBcdDisplay(data);
+    ASSERT_EQ(data[0], 0x0);
+    ASSERT_EQ(data[1], 0x0);
+    ASSERT_EQ(data[2], 0x0);
 }
 
 int main(int argc, char* argv[]) {
